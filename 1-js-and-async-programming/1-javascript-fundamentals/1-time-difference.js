dbe5cd5014ef7ab6599edbbdb99d516974438344
HarudesDevRv <luisrendon@ravn.co>
2026-01-06 16:15:46 -0500

    Challenge 1 initial solve

--- 1-js-and-async-programming/1-javascript-fundamentals/1-time-difference.js
+++ 1-js-and-async-programming/1-javascript-fundamentals/1-time-difference.js
@@ -16,6 +16,21 @@ timeDifference(7200, 3400); // Expected output: "01:03:20"
 
 */
 
-const timeDifference = (a, b) => {};
+const timeDifference = (a, b) => {
+    if(b>a){//ensure A is always greater than B or equal
+        [b,a] = [a,b];
+    }
+    let differenceInSeconds = a-b;
+    let hours = Math.trunc(differenceInSeconds/3600);//get total hours
+    differenceInSeconds %= 3600;
+    let minutes = Math.trunc(differenceInSeconds/60);//get total minutes
+    differenceInSeconds%=60;
+    let seconds = differenceInSeconds;//get seconds
+    //place a 0 where needed
+    let textualHours = hours<10?"0"+hours.toString():hours.toString();
+    let textualMinutes = minutes<10?"0"+minutes.toString():minutes.toString();
+    let textualSeconds = seconds<10?"0"+seconds.toString():seconds.toString();
+    return `${textualHours}:${textualMinutes}:${textualSeconds}`;//concatenate results with : as separator
+};
 
 module.exports = timeDifference;
