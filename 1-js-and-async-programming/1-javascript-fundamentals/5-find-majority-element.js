d684718bdfbeb162616c36d0e5df36293e7e5120
HarudesDevRv <luisrendon@ravn.co>
2026-01-06 18:05:54 -0500

    Challenge 5 initial solve

--- 1-js-and-async-programming/1-javascript-fundamentals/5-find-majority-element.js
+++ 1-js-and-async-programming/1-javascript-fundamentals/5-find-majority-element.js
@@ -19,6 +19,23 @@ findMajorityElement([1, 2, 3, 4, 5]); // Expected output: null
 
 */
 
-const findMajorityElement = (arr) => {};
+const findMajorityElement = (arr) => {
+    //use a map to store the count of each numeric value
+    let numbersCount = new Map();
+    for(let number of arr){
+        if(numbersCount.has(number)){
+            numbersCount.set(number,numbersCount.get(number)+1);
+        }
+        else{
+            numbersCount.set(number,1);
+        }
+    }
+    //look if there is a value on the map higher than arr length/2
+    for(let pair of numbersCount){
+        if(pair[1]>Math.floor(arr.length/2))
+            return pair[0];
+    }
+    return null;
+};
 
 module.exports = findMajorityElement;
